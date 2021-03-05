1f3970b52658bc19f55cb0df6f5a54609cfe0de2
Fauzan Abiyyu Dzaky <fabiyudzaky@yahoo.co.id>
2021-03-05 17:22:23 +0700

    variable

---
+++ variable.java
@@ -0,0 +1,39 @@
+package pbogab;
+
+public class variables {
+    public static void main(String[] args){
+        // tipe data
+        // integers int
+        // string
+        // float
+        // boolean
+        // setiap variable mewakili 1 tipe data
+
+        int angka;
+
+        angka = 1; //masukkan nilai 1 ke dalam variabel angka
+        System.out.println(angka);
+        angka = 10;
+
+        int angka;
+        int $angka;
+        System.out.println(angka);
+        char karakter;
+        karakter '&'; //variabel dengan tipe char value-nya diapit dengan single quote
+        char karakters;
+        karakters = 'b';
+        System.out.println(karakter);
+        double pecahan 0.5;
+        System.out.println(pecahan);
+        System.out.printf("Angka=%d \n", angka); // style orang C dan C++
+        System.out.println("Angka= "+pecahan+" adalah pecahan");
+        System.out.printf("Angka=%.2f", pecahan);
+        // angka = pecahan;
+        pecahan = angka;
+        System.out.printf("Angka =%.2f \n", pecahan);
+        int angka2 = 3;
+        double hasilbagi;
+        hasilbagi = (float)angka/(float)angka2;
+        System.out.println(hasilbagi);
+    }
+}
